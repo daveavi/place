6f1e983f1ec80fff566cc7655ae4ed11e552bd66
student <aj.lee@mail.utoronto.ca>
2019-11-24 17:49:17 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # csc409_a3
 r/place replica
+
+November 24th, 2019:
+    Things we did:
+        - Understood basics of project
+        - Set up git up for project
+        - Created docker-compose.yml so that we have a redis conatiner in communication with our server
+        - Server is able to connect to redis now because of it (we use docker stack deply to deploy our application)
--- place/server.js
+++ place/server.js
@@ -1,14 +1,40 @@
 const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 8081 });
+
+var redis = require('redis');
+var client = redis.createClient(6379, 'redis');
+
+client.on('connect', function() {
+	console.log('connected');
+});
+
+client.on("error", function (err) {
+    console.log("Error " + err);
+});
  
 var dim = 250; // note: this is not the right dimensions!!
 var board=new Array(dim);
+
+var s = "a";
+client.set('board', s.repeat(50));
 for(var x=0;x<dim;x++){
 	board[x]=new Array(dim);
 	for(var y=0;y<dim;y++){
+		//client.set('boardbits', 'u4', y*dim + x, 15);
 		board[x][y]={ 'r':255, 'g':255, 'b':255 };
 	}
 }
+client.get('board', redis.print);
+client.getbit('board', 0, redis.print);
+client.getbit('board', 1, redis.print);
+client.getbit('board', 2, redis.print);
+client.getbit('board', 3, redis.print);
+client.getbit('board', 4, redis.print);
+client.getbit('board', 5, redis.print);
+client.getbit('board', 6, redis.print);
+client.getbit('board', 7, redis.print);
+
+
 
 wss.on('close', function() {
     console.log('disconnected');
@@ -50,6 +76,15 @@ wss.on('connection', function(ws) {
 	// send initial board: this is slow!!!
 	for(x=0;x<dim;x++){
 		for(y=0;y<dim;y++){
+			client.get('board', redis.print);
+			client.getbit('board', 0, redis.print);
+			client.getbit('board', 1, redis.print);
+			client.getbit('board', 2, redis.print);
+			client.getbit('board', 3, redis.print);
+			client.getbit('board', 4, redis.print);
+			client.getbit('board', 5, redis.print);
+			client.getbit('board', 6, redis.print);
+			client.getbit('board', 7, redis.print);
 			var o = { 'x' : x, 'y' : y, 'r': board[x][y].r, 'g': board[x][y].g, 'b': board[x][y].b };
 			ws.send(JSON.stringify(o));
 		}
